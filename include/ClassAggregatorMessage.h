aec851b59e8e2594b0bb0e6eb06379d3642fafda
dsapienza <177992@studenti.unimore.it>
2020-04-17 17:46:53 +0200

    Add a new message class for the class-aggregator
    Signed-off-by: Davide Sapienza <sapienza.dav@gmail.com>

---
+++ include/ClassAggregatorMessage.h
@@ -0,0 +1,22 @@
+#ifndef CAMESSAGE_H
+#define CAMESSAGE_H
+
+#include <pthread.h>
+#include <assert.h>
+#include "../masa_protocol/include/messages.hpp"
+
+class ClassAggregatorMessage {
+protected:
+    std::vector<MasaMessage> message_list;
+    pthread_mutex_t mutex;
+    bool deleteOld(MasaMessage m);
+public:
+    ClassAggregatorMessage();
+    ~ClassAggregatorMessage();
+    void insertMessage(MasaMessage m);
+    std::vector<MasaMessage> getMessages();
+    
+    
+};
+
+#endif /* CAMESSAGE_H */
---
+++ src/ClassAggregatorMessage.cpp
@@ -0,0 +1,38 @@
+#include "ClassAggregatorMessage.h"
+
+ClassAggregatorMessage::ClassAggregatorMessage() {
+}
+
+ClassAggregatorMessage::~ClassAggregatorMessage() {
+}
+
+bool ClassAggregatorMessage::deleteOld(MasaMessage m) {
+    bool deleted = false;
+    auto it = message_list.begin();
+    while (it != message_list.end()) {
+        if (it->cam_idx == m.cam_idx && it->t_stamp_ms < m.t_stamp_ms) {
+            it = message_list.erase(it);
+            deleted = true;
+        }
+        else 
+            ++it;
+    }
+    return deleted;
+}
+
+void ClassAggregatorMessage::insertMessage(MasaMessage m) {
+    pthread_mutex_lock(&mutex);
+    if (deleteOld(m)) 
+        std::cout<<"some message are deleted -- you are too slow\n";
+    message_list.push_back(m);
+    pthread_mutex_unlock(&mutex);
+}
+
+std::vector<MasaMessage> ClassAggregatorMessage::getMessages() {
+    pthread_mutex_lock(&mutex);
+    std::vector<MasaMessage> copy_list = message_list;
+    message_list.clear();
+    pthread_mutex_unlock(&mutex);
+    assert(copy_list.size() != 0);
+    return copy_list;
+}
