9afaaba41f60922af1169775ad9610b4a46f3d6b
dsapienza <177992@studenti.unimore.it>
2020-04-27 12:47:34 +0200

    Add a first working demo
    The class-aggregator executable performs the server side task.
    the test-aggregatore executable performs the client side task.
    
    Signed-off-by: Davide Sapienza <sapienza.dav@gmail.com>

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,8 +1,50 @@
 #include <iostream>
+#include "utils.h"
+#include "configuration.h"
+#include "ClassAggregatorMessage.h"
+#include "Receiver.h"
+#include "Sender.h"
+#include "Deduplicator.h"
+#include "Aggregator.h"
 
+int main(int argc, char **argv) {
+    ClassAggregatorMessage received_messages;       // Receiver fills this message, Deduplicator deduplicates its objects
+    ClassAggregatorMessage deduplicated_messages;   // Deduplicator creates this message without duplicator, Aggregator adds some info
+    ClassAggregatorMessage aggregated_messages;     // Aggregator creates this message, Sender sends it to all dest.
 
-int main()
-{
-    std::cout<<"This is a test!"<<std::endl;
+    // read yaml parameters file
+    Parameters_t param;
+    if(!readParameters(argc, argv, &param)) {
+        exit(EXIT_SUCCESS);         // help
+    }
+
+    gRun = true;
+
+    // start the viewer
+    AggregatorViewer v(param.pngFile);
+    v.setWindowName("Map");
+    v.setBackground(tk::gui::color::DARK_GRAY);
+    v.initOnThread();
+    
+    // TODO: iterate on ports:
+    if (param.inputPortList.size() > 1)
+        std::cerr<<"many input ports: no supported. Use only the first...\n";
+    Receiver r(received_messages, param.inputPortList[0]);
+    r.start();
+
+    Deduplicator d(received_messages, deduplicated_messages, param.tifFile, v, param.visualization);
+    d.start();
+
+    Aggregator a(deduplicated_messages, aggregated_messages, v, param.visualization);
+    a.start();
+
+    Sender s(aggregated_messages, param.outputIpList, param.outputPortList);
+    s.start();
+
+    v.joinThread();
+    r.end();
+    d.end();
+    a.end();
+    s.end();
     return EXIT_SUCCESS;
 }
